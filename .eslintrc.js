e87d375882d959ba37be27fe05d8a873fed32035
sushidesu <hugemerge@gmail.com>
2021-02-28 16:54:37 +0900

    husky, lint-stagedを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    "prettier"
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }]
   },
-};
+}
