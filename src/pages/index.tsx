e9dcbd7905c2217ff14f65f6f56b060eb990860a
sushidesu <hugemerge@gmail.com>
2021-02-28 16:01:43 +0900

    ディレクトリを整理

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Cookie Clicker Maker</h1>
+        <p>start!!!</p>
+      </main>
+    </div>
+  )
+}
