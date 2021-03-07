357e11dbb874f09cf14fef1076c1021c5453a294
sushidesu <hugemerge@gmail.com>
2021-03-07 20:46:54 +0900

    モーダルの見た目を整えた

--- src/components/GameCreateForm.tsx
+++ src/components/GameCreateForm.tsx
@@ -15,44 +15,37 @@ export function GameCreateForm({
   onBackgroundColorBlur,
 }: Props) {
   return (
-    <div className="flex flex-col items-center p-4 w-full bg-white shadow-md">
-      <h1 className="text-xl font-bold">ゲームを作成</h1>
-      <form onSubmit={onSubmit} className="w-full space-y-4">
-        <div className="flex flex-col">
-          <label className="text-grey-darkest text-lg font-bold">
-            アイコン
-          </label>
-          <EmojiPicker onIconSelect={onIconSelect} />
-        </div>
-        <div className="flex flex-col">
-          <label className="text-grey-darkest text-lg font-bold">
-            ゲーム名
-          </label>
-          <input
-            className="text-grey-darkest px-3 py-2 border rounded"
-            type="text"
-            onBlur={onNameBlur}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label className="text-grey-darkest text-lg font-bold">作成者</label>
-          <input
-            className="text-grey-darkest px-3 py-2 border rounded"
-            type="text"
-            onBlur={onCreatedByBlur}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label className="text-grey-darkest text-lg font-bold">背景色</label>
-          <input type="color" onBlur={onBackgroundColorBlur} />
-        </div>
-        <button
-          className="block mx-auto px-4 py-2 text-white text-lg font-bold tracking-widest bg-teal-500 hover:bg-teal-600 rounded shadow-md"
-          type="submit"
-        >
-          ゲームを登録
-        </button>
-      </form>
-    </div>
+    <form onSubmit={onSubmit} className="w-full space-y-4">
+      <div className="flex flex-col">
+        <label className="text-grey-darkest text-lg font-bold">アイコン</label>
+        <EmojiPicker onIconSelect={onIconSelect} />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-grey-darkest text-lg font-bold">ゲーム名</label>
+        <input
+          className="text-grey-darkest px-3 py-2 border rounded"
+          type="text"
+          onBlur={onNameBlur}
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-grey-darkest text-lg font-bold">作成者</label>
+        <input
+          className="text-grey-darkest px-3 py-2 border rounded"
+          type="text"
+          onBlur={onCreatedByBlur}
+        />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-grey-darkest text-lg font-bold">背景色</label>
+        <input type="color" onBlur={onBackgroundColorBlur} />
+      </div>
+      <button
+        className="block mx-auto px-4 py-2 text-white text-lg font-bold tracking-widest bg-teal-500 hover:bg-teal-600 rounded shadow-md"
+        type="submit"
+      >
+        ゲームを登録
+      </button>
+    </form>
   )
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react"
+import Modal from "react-modal"
 import { Header } from "components/Header"
-import { Modal } from "components/Modal"
 import { GameCreateForm, Props as FormProps } from "components/GameCreateForm"
 
 export type Props = {
@@ -16,9 +16,16 @@ export function Layout({ children, ...rest }: Props) {
     <div className="overflow-hidden">
       <Header onPostClick={toggleModal} />
       <div className="pb-8">{children}</div>
-      <Modal show={show}>
-        <div>
-          <button onClick={toggleModal}>close</button>
+      <Modal
+        preventScroll
+        className="absolute inset-10 outline-none"
+        isOpen={show}
+      >
+        <div className="relative flex flex-col items-center mx-auto pb-8 pt-4 px-6 w-full max-w-xl bg-white shadow-md">
+          <button className="absolute right-2 top-2 p-2" onClick={toggleModal}>
+            閉じる
+          </button>
+          <h1 className="mb-4 text-xl font-bold">ゲームを作成</h1>
           <GameCreateForm {...rest} />
         </div>
       </Modal>
