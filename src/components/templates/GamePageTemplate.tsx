971a3dca479f4d3c862a959f1854f6b4c41135af
sushidesu <hugemerge@gmail.com>
2021-03-07 17:18:39 +0900

    GamePageTemplateを調整

--- src/components/templates/GamePageTemplate.tsx
+++ src/components/templates/GamePageTemplate.tsx
@@ -10,9 +10,8 @@ export function GamePageTemplate({ game }: Props) {
   return (
     <Layout>
       <main>
-        <div>
-          <h1>Game Page</h1>
-          <div className="flex flex-col items-center mt-4 mx-auto p-6 max-w-sm bg-white rounded-xl shadow-md cursor-pointer space-y-2">
+        <div className="mt-24">
+          <div className="ronded-xl flex flex-col items-center mt-4 mx-auto p-6 max-w-sm bg-white shadow-md space-y-2">
             <p className="font-semibold">{game.name}</p>
             <Emoji emoji={game.icon} set="twitter" size={64} />
             <p>{`作成者: ${game.createdBy}`}</p>
