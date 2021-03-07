392598c76e3dee351f86d75c6c9a1089d66c33db
sushidesu <hugemerge@gmail.com>
2021-03-07 17:57:00 +0900

    クッキーを焼けるようにした

--- src/components/containers/GamePageContainer.tsx
+++ src/components/containers/GamePageContainer.tsx
@@ -1,18 +1,26 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { useRouter } from "next/router"
 import { Game } from "domain/entity"
 import { database } from "plugins/firebaseApp"
 import { GameRepository } from "infrastructure/gameRepository"
 import { ApplicationValueRepository } from "infrastructure/applicationValueRepository"
-import { GamePageTemplate } from "components/templates/GamePageTemplate"
+import {
+  GamePageTemplate,
+  Props as TemplateProps,
+} from "components/templates/GamePageTemplate"
 import { GameNotFoundTemplate } from "components/templates/GameNotFoundTemplate"
 
 export function GamePageContainer() {
   const [game, setGame] = useState<Game | null | undefined>(null)
+  const [cookies, setCookies] = useState(0)
   const router = useRouter()
   const { gid } = router.query
   const appValueRepository = new ApplicationValueRepository(database)
   const gameRepository = new GameRepository(database, appValueRepository)
+
+  const bakeCookie = useCallback(() => {
+    setCookies((prev) => prev + 1)
+  }, [setCookies])
 
   useEffect(() => {
     let canceled = false
@@ -30,7 +38,12 @@ export function GamePageContainer() {
   }, [gid])
 
   if (game) {
-    return <GamePageTemplate game={game} />
+    const props: TemplateProps = {
+      game,
+      onCookieClick: bakeCookie,
+      cookies,
+    }
+    return <GamePageTemplate {...props} />
   } else {
     return <GameNotFoundTemplate />
   }
--- src/components/templates/GamePageTemplate.tsx
+++ src/components/templates/GamePageTemplate.tsx
@@ -4,16 +4,24 @@ import { Game } from "domain/entity"
 
 export type Props = {
   game: Game
+  cookies: number
+  onCookieClick: () => void
 }
 
-export function GamePageTemplate({ game }: Props) {
+export function GamePageTemplate({ game, cookies, onCookieClick }: Props) {
   return (
     <Layout>
       <main>
         <div className="mt-24">
           <div className="ronded-xl flex flex-col items-center mt-4 mx-auto p-6 max-w-sm bg-white shadow-md space-y-2">
             <p className="font-semibold">{game.name}</p>
-            <Emoji emoji={game.icon} set="twitter" size={64} />
+            <Emoji
+              onClick={onCookieClick}
+              emoji={game.icon}
+              set="twitter"
+              size={64}
+            />
+            <p>{cookies}</p>
             <p>{`作成者: ${game.createdBy}`}</p>
           </div>
         </div>
